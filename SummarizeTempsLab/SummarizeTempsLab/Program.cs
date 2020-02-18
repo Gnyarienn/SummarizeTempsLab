bc709b66cab42d3f385bbb0198e2efafc358b19c
Gnyarienn <59981857+Gnyarienn@users.noreply.github.com>
2020-02-17 21:13:13 -0600

    Update Program.cs

--- SummarizeTempsLab/SummarizeTempsLab/Program.cs
+++ SummarizeTempsLab/SummarizeTempsLab/Program.cs
@@ -7,7 +7,73 @@ namespace SummarizeTempsLab
     {
         static void Main(string[] args)
         {
-            // temperature data is in temps.txt
+            
+
+            
+            string filename;
+            Console.WriteLine("Enter file name");
+            filename = Console.ReadLine();
+
+            if (File.Exists(filename))
+            {
+                Console.WriteLine("file exists");
+                //ask user to enter temperature
+                Console.WriteLine("Enter treshold");
+                string input;
+                input = Console.ReadLine();
+                int threshold = int.Parse(input);
+
+
+                int sumTemps = 0;
+                int numabove = 0;
+                int numbelow = 0;
+
+
+                //open file and create stream reader
+                // read a line into the string variable
+                using (StreamReader sr = File.OpenText(filename))
+                {
+                    string line = sr.ReadLine();
+                    int temp;
+
+                    while (line != null)
+                    {
+                        temp = int.Parse(line);
+                        sumTemps += temp;
+                        if (threshold <= temp) 
+                        {
+                            numabove += 1;
+
+                        }
+                        else
+                        {
+                            numbelow += 1;
+                        }
+
+                        line = sr.ReadLine();
+
+                    }
+                    Console.WriteLine("number of temperatures above the treshold are" + numabove);
+                    Console.WriteLine("number of temperatures below the treshold are" + numbelow);
+                    int average;
+                    average = sumTemps / (numabove + numbelow);
+                    Console.WriteLine("Average is " + average);
+
+
+
+                        
+
+
+
+
+
+                }
+
+
+            }
+            
+           
+
         }
     }
 }
